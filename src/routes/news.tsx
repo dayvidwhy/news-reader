9d92b00c5e9d2faa3d02a1cdce820a1729d16dca
dayvidwhy <dayvidwhy@gmail.com>
2024-04-07 11:50:26 +1000

    List of stories.

--- src/routes/news.tsx
+++ src/routes/news.tsx
@@ -1,24 +1,33 @@
 import { A } from "@solidjs/router";
 import { createAsync, cache } from "@solidjs/router";
+import { format } from "date-fns";
 
-const getNewsStories = cache(async () => {
+interface NewsStory {
+    title: string;
+    url: string;
+    id: number;
+    score: number;
+    time: number;
+    descendants: number;
+};
+
+const getNewsStories = cache(async (): Promise<NewsStory[]> => {
     "use server";
     const response = await fetch("https://hacker-news.firebaseio.com/v0/topstories.json");
     const storyIds = await response.json();
 
     const storiesDetails = await Promise.all(storyIds.map((id: number) => {
-        return new Promise<{
-            title: string;
-            url: string;
-            id: number;
         // eslint-disable-next-line no-async-promise-executor
-        }>(async (resolve): Promise<void> => {
+        return new Promise<NewsStory>(async (resolve): Promise<void> => {
             const story = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
             const storyDetails = await story.json();
             resolve({
                 title: storyDetails.title,
                 url: storyDetails.url,
-                id: storyDetails.id
+                id: storyDetails.id,
+                score: storyDetails.score,
+                time: storyDetails.time,
+                descendants: storyDetails.descendants
             });
         });
     }));
@@ -32,16 +41,25 @@ export const route = {
 export default function News() {
     const newsStories = createAsync(() => getNewsStories());
     return (
-        <main class="container">
-            <h1>
-                News
+        <main class="container pt-2 mx-auto">
+            <h1 class="text-4xl">
+                Top Stories
             </h1>
-            <ul class="container mx-auto">
+            <ul class="container flex flex-col">
                 {newsStories() && newsStories()?.map((story) => (
-                    <li class="my-4">
-                        <h1>{story.title}</h1>
-                        <p>{story.url}</p>
-                        <A href={`/news/${story.id}`}>{story.id}</A>
+                    <li class="m-2 bg-slate-100 p-2 flex flex-col">
+                        <a class="text-xl hover:text-slate-800 hover:underline" href={story.url}>{story.title}</a>
+                        <p>
+                            <span class="text-slate-500 pr-1 text-xs">
+                                {`${story.score}pts - `}
+                            </span>
+                            <span class="text-slate-500 text-xs">
+                                {format(new Date(story.time), "pp").toLowerCase()}
+                            </span>
+                            <A href={`/news/${story.id}`} class="text-slate-500 text-xs pl-1 hover:underline">
+                                {`${story.descendants || "0"} ${story.descendants !== 1 ? "comments" : "comment"}`}
+                            </A>
+                        </p>
                     </li>
                 ))}
             </ul>
