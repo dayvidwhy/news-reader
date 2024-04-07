3dfb8cbdfbf271a98a0a2f3b4cfe73390047fe1d
dayvidwhy <dayvidwhy@gmail.com>
2024-04-07 19:29:43 +1000

    Fix style.

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -5,12 +5,10 @@ import type { CommentTree } from "~/utils/api";
 export const Comments = (commentData: CommentTree) => {
     const [visibile, setVisibile] = createSignal(true);
     return (
-        <div class="m-2 pl-2 pt-2 bg-slate-100 border-l border-slate-200 flex">
-            <div class="flex flex-col justify-start text-center">
-                <button class="px-1 w-5 bg-slate-200 mr-1" onClick={[() => setVisibile((prev) => !prev), null]}>
-                    {visibile() ? "-" : "+"}
-                </button>
-            </div>
+        <div class={"m-2 pl-2 bg-slate-100 border-l border-slate-200"}>
+            <button class={`text-xs bg-slate-200 ${visibile() ? "mb-1" : ""}`} onClick={[() => setVisibile((prev) => !prev), null]}>
+                {visibile() ? "hide" : "show"}
+            </button>
             <Show when={visibile()}>
                 <div>
                     <p class="text-xs text-wrap break-words">
