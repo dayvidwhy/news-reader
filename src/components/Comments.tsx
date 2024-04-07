01c9b6e23c5ba799858231294477826078192450
dayvidwhy <dayvidwhy@gmail.com>
2024-04-07 19:14:45 +1000

    Add visibility toggle for comments.

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,20 +1,30 @@
-import { For } from "solid-js";
+import { For, Show, createSignal } from "solid-js";
 import { format } from "date-fns";
 import type { CommentTree } from "~/utils/api";
 
 export const Comments = (commentData: CommentTree) => {
+    const [visibile, setVisibile] = createSignal(true);
     return (
-        <div class="m-2 pl-2 pt-2 bg-slate-100 border-l border-slate-200">
-            <p class="text-xs text-wrap break-words">
-                {commentData.comment.text}
-            </p>
-            <p class="text-slate-500 text-xs">
-                <span>{`By ${commentData.comment.by}`}</span>
-                <span class="pl-1">{`at ${format(new Date(commentData.comment.time), "pp").toLowerCase()}`}</span>
-            </p>
-            <div class="pl">
-                {<For each={commentData.kids}>{Comments}</For>}
+        <div class="m-2 pl-2 pt-2 bg-slate-100 border-l border-slate-200 flex">
+            <div class="flex flex-col justify-start text-center">
+                <button class="px-1 w-5 bg-slate-200 mr-1" onClick={[() => setVisibile((prev) => !prev), null]}>
+                    {visibile() ? "-" : "+"}
+                </button>
             </div>
+            <Show when={visibile()}>
+                <div>
+                    <p class="text-xs text-wrap break-words">
+                        {commentData.comment.text}
+                    </p>
+                    <p class="text-slate-500 text-xs">
+                        <span>{`By ${commentData.comment.by}`}</span>
+                        <span class="pl-1">{`at ${format(new Date(commentData.comment.time), "pp").toLowerCase()}`}</span>
+                    </p>
+                    <div class="pl">
+                        {<For each={commentData.kids}>{Comments}</For>}
+                    </div>
+                </div>
+            </Show>
         </div>
     );
 };
